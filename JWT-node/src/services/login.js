4a921e4f02339e4d6982332733d5cda7dcc89c28
anusha mahajan <99132058+anushamahajan5@users.noreply.github.com>
2024-11-30 15:37:36 +0530

    Update login.js

--- JWT-node/src/services/login.js
+++ JWT-node/src/services/login.js
@@ -9,7 +9,7 @@ async function login(email, password) {
         if(!existingUser){
             throw new Error("User not found");
         }
-        const isPasswordValid = bcrypt.compare(password, existingUser.password);
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
         if(!isPasswordValid){
             throw new Error("Incorrect Password");
         }
